6e62af81cbc07b9ca11f385fa3a29d3f354d52eb
James Tolley <james@altitudeangel.com>
2025-12-03 17:27:06 +0000

    More concise, but less efficient.

--- AdventOfCode2025/Day3/Program.cs
+++ AdventOfCode2025/Day3/Program.cs
@@ -1,4 +1,5 @@
-﻿var sum = File.ReadAllLines("input.txt")
+﻿var sum = File
+    .ReadAllLines("input.txt")
     .Sum(l => FindJoltage(l, 12));
 
 Console.WriteLine($"Sum: {sum}");
@@ -14,27 +15,12 @@ static long FindJoltage(string line, int numBatteries)
     for (var i = numBatteries - 1; i >= 0; i--)
     {
         var substr = str[..^i];
-        batteries[batteryIndex++] = FindHighest(substr, out var index);
+        batteries[batteryIndex] = substr.Max();
+        var index = substr.IndexOf(batteries[batteryIndex++]);
         str = str[(index + 1)..];
     }
 
     var result = new string(batteries);
     Console.WriteLine($"{line} - {result}");
     return long.Parse(result);
-
-}
-
-static char FindHighest(string str, out int index)
-{
-    for (var i = 9; i > 0; i--)
-    {
-        var idx = str.IndexOf(i.ToString(), StringComparison.Ordinal);
-        if (idx >= 0)
-        {
-            index = idx;
-            return str[idx];
-        }
-    }
-
-    throw new InvalidOperationException("Should not reach here");
 }
