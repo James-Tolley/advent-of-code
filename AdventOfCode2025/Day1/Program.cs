36297eb965c71da1fcd548f40c1f978e7b7ffc27
James Tolley <james@altitudeangel.com>
2025-12-03 11:50:22 +0000

    Cleanup

--- AdventOfCode2025/Day1/Program.cs
+++ AdventOfCode2025/Day1/Program.cs
@@ -1,15 +1,7 @@
-﻿using BenchmarkDotNet.Running;
-BenchmarkRunner.Run<DialBenchmark>();
+﻿
+var dial = new Dial(50, 100);
+var lines = File.ReadAllLines("input.txt");
+    
+dial.ExecuteSequence(lines);
 
-//Run();
-//return 0;
-
-static void Run()
-{
-    var dial = new Dial(50, 100);
-    var lines = File.ReadAllLines("input.txt");
-        
-    dial.ExecuteSequence(lines);
-
-    Console.WriteLine($"Zeroes: {dial.Zeroes}");
-}
+Console.WriteLine($"Zeroes: {dial.Zeroes}");
