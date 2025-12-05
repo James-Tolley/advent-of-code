1833f11dcbc052e1f9ea41362d1bd3bf692a9cb2
James Tolley <james@altitudeangel.com>
2025-12-05 13:03:08 +0000

    Part 2

--- AdventOfCode2025/Day5/Program.cs
+++ AdventOfCode2025/Day5/Program.cs
@@ -1,32 +1,101 @@
-﻿
-var ranges = new List<long[]>();
+﻿var fresh = await CountAllFresh("input.txt");
 
-var fs = File.OpenText("input.txt");
-var line = await fs.ReadLineAsync();
+Console.WriteLine($"Fresh ingredients: {fresh}");
+return 0;
 
-Console.WriteLine("Populating ranges...");
-while (string.IsNullOrEmpty(line) == false)
+// Part1
+async Task<long> CountAvailableFresh(string filename)
 {
-    var range = line.Split('-').Select(long.Parse).ToArray();
-    ranges.Add(range);
-    Console.WriteLine($"{range[0]}-{range[1]}");
-    line = await fs.ReadLineAsync();
-}
+    var fs = File.OpenText(filename);
+    var ranges = await ReadRanges(fs);
 
-var countFresh = 0;
-line = await fs.ReadLineAsync();
-while (string.IsNullOrEmpty(line) == false)
-{
-    var id = long.Parse(line);
-    foreach (var range in ranges)
+    var countFresh = 0;
+    var line = await fs.ReadLineAsync();
+    while (string.IsNullOrEmpty(line) == false)
     {
-        if (id >= range[0] && id <= range[1])
+        var id = long.Parse(line);
+
+        if (ranges.FirstOrDefault(r => id >= r[0] && id <= r[1]) is not null)
         {
             countFresh++;
-            break;
         }
+
+        line = await fs.ReadLineAsync();
     }
-    line = await fs.ReadLineAsync();
+
+    return countFresh;
 }
 
-Console.WriteLine($"Fresh ingredients: {countFresh}");
+// Part2
+async Task<long> CountAllFresh(string filename)
+{
+    var fs = File.OpenText(filename);
+    var ranges = await ReadRanges(fs);
+    ranges = TrimRanges(ranges);
+
+    return ranges.Sum(r => r[1] - r[0] + 1);
+}
+
+async Task<List<long[]>> ReadRanges(StreamReader file)
+{
+    var ranges = new List<long[]>();
+    var line = await file.ReadLineAsync();
+
+    while (string.IsNullOrEmpty(line) == false)
+    {
+        var range = line.Split('-').Select(long.Parse).ToArray();
+        ranges.Add(range);
+        line = await file.ReadLineAsync();
+    }
+
+    return ranges;
+}
+
+List<long[]> TrimRanges(List<long[]> ranges)
+{
+    var trimmedRanges = new List<long[]>();
+    var rangesTrimmed = false;
+    foreach (var range in ranges)
+    {
+        var currentRange = new long[] { range[0], range[1] };
+        List<long[]> newRanges = new();
+
+        foreach (var testRange in trimmedRanges)
+        {
+            if (currentRange[0] <= testRange[1] && currentRange[1] >= testRange[0])
+            {
+                // Range overlap
+                rangesTrimmed = true;
+
+                if (currentRange[0] >= testRange[0])
+                {
+                    currentRange[0] = testRange[1] + 1;
+                }
+
+                if (currentRange[1] <= testRange[1])
+                {
+                    currentRange[1] = testRange[0] - 1;
+                }
+
+                if (currentRange[0] < testRange[0] && currentRange[1] > testRange[1])
+                {
+                    // middle overlap - create a second range
+                    long[] newRange = [testRange[1] + 1, currentRange[1]];
+                    newRanges.Add(newRange);
+                    currentRange[1] = testRange[0] - 1;
+                } 
+            }
+        }
+
+        trimmedRanges.AddRange(newRanges);
+        if (currentRange[1] >= currentRange[0])
+        {
+            trimmedRanges.Add(currentRange);
+        }
+    }
+
+    return rangesTrimmed ? TrimRanges(trimmedRanges) : trimmedRanges;
+}
+
+
+
